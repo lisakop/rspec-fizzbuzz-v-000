e1278ef6c2b6657336617d7fd99fb2c5ca34f4d9
Lisa Koperski <lisa_koperski_junk@yahoo.com>
2017-10-30 21:51:07 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,15 +1,11 @@
 def fizzbuzz(fizz_int)
    if (fizz_int / 3 && fizz_int / 5) == Integer
         puts "FizzBuzz"
-    break
    elsif fizz_int / 3 == Integer
       puts "Fizz"
-   break
    elsif fizz_int / 5 == Integer
       puts "Buzz"
-   break
    else (fizz_int / 3 || fizz_int / 5) == !Integer
       puts nil
     end
-  end
 end
